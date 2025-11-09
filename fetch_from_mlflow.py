972a0f114087351017f6f16c4319954cabab87bb
sagar-mahurkar <sagar.0.mahurkar@gmail.com>
2025-11-09 09:12:23 +0000

    Added mlflow

--- fetch_from_mlflow.py
+++ fetch_from_mlflow.py
@@ -3,7 +3,7 @@ import sys
 import mlflow
 from mlflow.tracking import MlflowClient
 
-URI = "http://35.223.244.50:5000/"
+URI = "http://34.31.11.39:5000/"
 NAME = "iris-random-forest"
 PATH = "downloaded_models"
 
