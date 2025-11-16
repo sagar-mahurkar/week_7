e04370971bf6a1e39ff0eac7ed67ac469db26d1f
Sagar Mahurkar <sagar.0.mahurkar@gmail.com>
2025-11-16 19:01:35 +0530

    Update fetch_from_mlflow.py

--- fetch_from_mlflow.py
+++ fetch_from_mlflow.py
@@ -5,7 +5,7 @@ from mlflow.tracking import MlflowClient
 from mlflow.exceptions import MlflowException
 
 # -------------------------- Configuration --------------------------
-MLFLOW_TRACKING_URI = "http://34.170.88.52:5000"
+MLFLOW_TRACKING_URI = "http://34.122.49.196:5000"
 MODEL_NAME = "iris-random-forest"
 DOWNLOAD_DIR = "downloaded_models"
 LOCAL_MODEL_PATH = os.path.join(DOWNLOAD_DIR, "model.pkl")
